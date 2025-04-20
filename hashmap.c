2f7226957f14a57b98ef0790daadf7457003221d
Your Name <you@example.com>
2025-04-20 16:47:58 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -138,8 +138,15 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+    if (map == NULL) return NULL;
+    if (map->size == 0) return NULL;
+    for (int i = 0; i < map->capacity; i++)
+    {
+        if (map->buckets[i] != NULL) {
+            map->current = i;
+            return map->buckets[i];
+        }
+    }
 }
 
 Pair * nextMap(HashMap * map) {
